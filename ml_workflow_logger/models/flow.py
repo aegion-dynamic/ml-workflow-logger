dd4a4b70a87d7f0d7cd93457b4cff8b20bf2bada
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2024-08-02 18:40:07 +0530

    Added a datamodel for the the mongodb connection

---
+++ ml_workflow_logger/models/flow.py
@@ -0,0 +1,8 @@
+import uuid
+from pydantic import BaseModel, Field
+
+
+class Flow(BaseModel):
+    id: str = Field(alias='_id', default_factory=uuid.uuid4)
+    name: str = Field(alias='name')
+    pass
